f715452c020c8c67794d0dfbf1ecefbd85f84191
vin200118 <padekarvinayak5@gmail.com>
2019-11-09 00:28:46 +0530

    first commit

---
+++ src/main/java/com/example/demo/RestApiController.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.websystique.springboot.model.User;
+import com.websystique.springboot.service.UserService;
+import com.websystique.springboot.util.CustomErrorType;
+
+@RestController
+@RequestMapping("/api")
+public class RestApiController {
+
+	public static final Logger logger = LoggerFactory.getLogger(RestApiController.class);
+
+	@RequestMapping(value = "/abc", method = RequestMethod.GET)
+	public ResponseEntity<String> getRestAPI() {
+		
+		return new ResponseEntity<String>("this  is vinayak", HttpStatus.OK);
+	}
+}

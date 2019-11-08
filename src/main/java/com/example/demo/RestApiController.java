4eec8fa24dc9f3163f5e26e0a0e5379539ded1d0
vin200118 <padekarvinayak5@gmail.com>
2019-11-09 01:23:14 +0530

    first commit

--- src/main/java/com/example/demo/RestApiController.java
+++ src/main/java/com/example/demo/RestApiController.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
+import org.postgresql.jdbc3.Jdbc3SimpleDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +23,15 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RestController
 @RequestMapping("/api")
 public class RestApiController {
-
+@Autowired
+JdbcTemplate jdbcTemplate;
 	public static final Logger logger = LoggerFactory.getLogger(RestApiController.class);
 
 	@RequestMapping(value = "/abc", method = RequestMethod.GET)
 	public ResponseEntity<String> getRestAPI() {
+		
+			jdbcTemplate.execute("create table employee (id int, name varchar)");
+			jdbcTemplate.execute("insert into employee (id, name) values (1, 'A')");
 		
 		return new ResponseEntity<String>("this  is vinayak", HttpStatus.OK);
 	}

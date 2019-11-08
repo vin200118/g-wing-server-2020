92cc802361435236e0c240dfe4ee32df3941010a
vin200118 <padekarvinayak5@gmail.com>
2019-11-09 01:31:46 +0530

    first commit

--- src/main/java/com/example/demo/RestApiController.java
+++ src/main/java/com/example/demo/RestApiController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 import org.postgresql.jdbc3.Jdbc3SimpleDataSource;
 import org.slf4j.Logger;
@@ -30,9 +31,15 @@ JdbcTemplate jdbcTemplate;
 	@RequestMapping(value = "/abc", method = RequestMethod.GET)
 	public ResponseEntity<String> getRestAPI() {
 		
-			jdbcTemplate.execute("create table employee (id int, name varchar)");
-			jdbcTemplate.execute("insert into employee (id, name) values (1, 'A')");
-		
-		return new ResponseEntity<String>("this  is vinayak", HttpStatus.OK);
+			//jdbcTemplate.execute("create table employee (id int, name varchar)");
+			//jdbcTemplate.execute("insert into employee (id, name) values (1, 'A')");  
+			List<Map<String, Object>> rows = jdbcTemplate.queryForList("select count(*) from employee");
+			String id = null;
+			String name = null;
+			 for (Map row : rows) {
+				 id = (String) row.get("id");
+				 name =  (String) row.get("name");
+			 }
+		return new ResponseEntity<String>(" Id : "+id+", Name: "+name, HttpStatus.OK);
 	}
 }

0dc46b9f09cb30ef1b1196abe434f290ef803cf8
vin200118 <padekarvinayak5@gmail.com>
2019-11-09 14:24:54 +0530

    first commit

--- src/main/java/com/example/demo/RestApiController.java
+++ src/main/java/com/example/demo/RestApiController.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -13,6 +16,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,13 +24,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/")
 public class RestApiController {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
+	SimpleDateFormat formatter = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");  
 	public static final Logger logger = LoggerFactory.getLogger(RestApiController.class);
+	String tableName="cs_event";
 
 	@RequestMapping(value = "delete-table/{tableName}", method = RequestMethod.GET)
 	public String deleteTable(@PathVariable String tableName) {
@@ -36,16 +42,29 @@ public class RestApiController {
 	
 	@RequestMapping(value = "/create-table/{tableName}", method = RequestMethod.GET)
 	public String getRestAPI(@PathVariable String tableName) {
-			jdbcTemplate.execute("create table "+tableName+" (id int, name varchar)");
-		return "tableCreated";
+			jdbcTemplate.execute("create table "+tableName+" (opt VARCHAR (10) PRIMARY KEY,"
+															+ "registration VARCHAR (30), "
+															+ "gift VARCHAR (30),)"
+															+ "lunch VARCHAR (30) ");
+		return "table created";
 	}
 	
-	@RequestMapping(value = "/process/{processName}", method = RequestMethod.GET)
-	public List<Map<String, Object>> processName(@PathVariable String processName) {
-		
-			jdbcTemplate.execute("insert into employee (id, name) values (1, 'Vinayak123')");  
+	@RequestMapping(value = "/process/{processName}/{opt}", method = RequestMethod.GET)
+	public List<Map<String, Object>> processName(@PathVariable String processName,@PathVariable String opt) {
+		  	Date date = new Date();  
+		   jdbcTemplate.execute("update "+tableName+" set "+processName+"="+formatter.format(date)+" where otp="+opt);  
 			List<Map<String, Object>> rows = jdbcTemplate.queryForList("select * from employee");
 			
+		return rows;
+	}
+	@RequestMapping(value = "/data/{processName}", method = RequestMethod.GET)
+	public List<Map<String, Object>> getData(@PathVariable String processName) {
+		List<Map<String, Object>> rows = new ArrayList<>();
+			if("all".equalsIgnoreCase(processName)) {
+			  	 rows = jdbcTemplate.queryForList("select * from "+tableName+"");
+			}else {
+				rows = jdbcTemplate.queryForList("select * from "+tableName+" where "+processName+" IS NOT NULL");
+			}
 		return rows;
 	}
 }

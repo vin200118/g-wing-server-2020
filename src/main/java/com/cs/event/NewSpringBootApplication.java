a84bbc5613a26dd4638c01fa5c6f4f98425a18f2
vin200118 <padekarvinayak5@gmail.com>
2019-11-09 23:43:50 +0530

    changes

--- src/main/java/com/cs/event/NewSpringBootApplication.java
+++ src/main/java/com/cs/event/NewSpringBootApplication.java
@@ -3,7 +3,7 @@ package com.cs.event;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={"com.example.demo"})
+@SpringBootApplication(scanBasePackages={"com.cs.event"})
 public class NewSpringBootApplication {
 	
 	public static void main(String[] args) {

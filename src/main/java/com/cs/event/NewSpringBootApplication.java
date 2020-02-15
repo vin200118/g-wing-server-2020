cdf066706c6825b5e86f020b7c0e5dc24615cb60
vin200118 <padekarvinayak5@gmail.com>
2020-02-15 14:58:35 +0530

    package scan

--- src/main/java/com/cs/event/NewSpringBootApplication.java
+++ src/main/java/com/cs/event/NewSpringBootApplication.java
@@ -3,7 +3,7 @@ package com.cs.event;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={"com.cs.event"})
+@SpringBootApplication(scanBasePackages={"com.cs"})
 public class NewSpringBootApplication {
 	
 	public static void main(String[] args) {

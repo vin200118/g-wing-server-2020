844c4ca1519afca85432d02512693413cd819446
vin200118 <padekarvinayak5@gmail.com>
2020-02-22 12:00:04 +0530

    changes

--- src/main/java/com/gwing/eventcontribution/EventContributionService.java
+++ src/main/java/com/gwing/eventcontribution/EventContributionService.java
@@ -41,9 +41,9 @@ public class EventContributionService {
 				 logger.info("db flatno : "+eventContriDetails.get("flatNo"));
 				 if(!flatNos.contains(""+eventContriDetails.get("flatNo"))) {
 					 logger.info("eventContriPaidAmount : "+eventContriDetails.get("eventContriPaidAmount"));
-					 if(eventContriDetails.get("eventContriPaidAmount") != null && 
-							 eventContriDetails.get("eventContriPaidAmount") != "" &&
-							 	eventContriDetails.get("eventContriPaidAmount") != "null"){
+					 if(null != eventContriDetails.get("eventContriPaidAmount") && 
+							 !"".equals(""+eventContriDetails.get("eventContriPaidAmount")) &&
+							 !"null".equals(""+eventContriDetails.get("eventContriPaidAmount")) ){
 						 throw new IllegalArgumentException("This"+eventContriDetails.get("flatNo")+" is already paid contirbution for this event,"
 						 		+ " you first informed to respective flat number and mark empty in contri receive page");
 					 }
